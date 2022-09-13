9369080af5f79fd0135a85474aacfeec9653e865
FerreiraLucasL <lukasm4a1@gmail.com>
2022-09-13 14:00:42 -0300

    Update PersonaController.java
    persona ya devuelve una persona y no una lista de personas, ya que no es necesario.

--- backEnd/src/main/java/com/portfolio/backend/controller/PersonaController.java
+++ backEnd/src/main/java/com/portfolio/backend/controller/PersonaController.java
@@ -3,14 +3,13 @@ package com.portfolio.backend.controller;
 
 import com.portfolio.backend.entity.Persona;
 import com.portfolio.backend.interfaces.IPersonaServ;
-import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -18,19 +17,20 @@ import org.springframework.web.bind.annotation.RestController;
 public class PersonaController {
     @Autowired IPersonaServ iPersonaServ;
     
-    @GetMapping("/persona/get")
+    @GetMapping("/persona/get/{id}")
     @CrossOrigin(origins = "http://localhost:4200")
-    //public ResponseEntity<Persona> ver... 
-    public List<Persona>  getPersona(){
-        return iPersonaServ.getPersona();
+    public ResponseEntity<Persona> getPersona (@PathVariable("id")Long id){
+        Persona persona = iPersonaServ.findPersona(id);
+        return new ResponseEntity<>(persona, HttpStatus.OK);
     }
     
-    /*@PreAuthorize("hasRole('ADMIN')")
+    
+    /* @PreAuthorize("hasRole('ADMIN')")
     @PostMapping("/persona/create")
     public String createPersona(@RequestBody Persona persona){
         iPersonaServ.savePersona(persona);
         return "persona creada con exito";
-    } */      
+    }       */
     
     //@PreAuthorize("hasRole('ADMIN')")
     @PutMapping("/persona/edit/{id}")
