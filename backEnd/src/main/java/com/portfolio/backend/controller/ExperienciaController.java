2adf35422fe08e7c5162c26b6f43417b6f29f671
FerreiraLucasL <lukasm4a1@gmail.com>
2022-09-13 13:14:44 -0300

    commit previo a modificaciones en persona

--- backEnd/src/main/java/com/portfolio/backend/controller/ExperienciaController.java
+++ backEnd/src/main/java/com/portfolio/backend/controller/ExperienciaController.java
@@ -5,6 +5,7 @@ import com.portfolio.backend.entity.Experiencia;
 import com.portfolio.backend.interfaces.IExperienciaServ;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -19,6 +20,7 @@ public class ExperienciaController {
     @Autowired IExperienciaServ iexperienciaServ;
     
     @GetMapping("/experiencia/get")
+    @CrossOrigin(origins = "http://localhost:4200")
     public List<Experiencia> getExperiencia(){
         return iexperienciaServ.getExperiencia();
     }
--- backEnd/src/main/java/com/portfolio/backend/controller/PersonaController.java
+++ backEnd/src/main/java/com/portfolio/backend/controller/PersonaController.java
@@ -5,7 +5,7 @@ import com.portfolio.backend.entity.Persona;
 import com.portfolio.backend.interfaces.IPersonaServ;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -19,16 +19,18 @@ public class PersonaController {
     @Autowired IPersonaServ iPersonaServ;
     
     @GetMapping("/persona/get")
+    @CrossOrigin(origins = "http://localhost:4200")
+    //public ResponseEntity<Persona> ver... 
     public List<Persona>  getPersona(){
         return iPersonaServ.getPersona();
     }
     
-    //@PreAuthorize("hasRole('ADMIN')")
+    /*@PreAuthorize("hasRole('ADMIN')")
     @PostMapping("/persona/create")
     public String createPersona(@RequestBody Persona persona){
         iPersonaServ.savePersona(persona);
         return "persona creada con exito";
-    }       
+    } */      
     
     //@PreAuthorize("hasRole('ADMIN')")
     @PutMapping("/persona/edit/{id}")
