4606d6ba450aa43ffe5de553817de5cb5934594e
FerreiraLucasL <lukasm4a1@gmail.com>
2022-10-29 16:11:27 -0300

    final comit

--- backEnd/src/main/java/com/portfolio/backend/controller/ExperienciaController.java
+++ backEnd/src/main/java/com/portfolio/backend/controller/ExperienciaController.java
@@ -7,7 +7,6 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -15,7 +14,6 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -35,7 +33,6 @@ public class ExperienciaController {
     //@CrossOrigin(origins = "https://portfoliolucasferreira.web.app/") 
     @CrossOrigin(origins = "http://localhost:4200") 
     public ResponseEntity<Experiencia> createExperiencia(@RequestBody Experiencia exp){
-        System.out.print(exp);
         iexperienciaServ.saveExperiencia(exp);
         return new ResponseEntity<>(exp, HttpStatus.OK);
     }
--- backEnd/src/main/java/com/portfolio/backend/controller/PersonaController.java
+++ backEnd/src/main/java/com/portfolio/backend/controller/PersonaController.java
@@ -11,7 +11,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -24,39 +23,26 @@ public class PersonaController {
     public ResponseEntity<Persona> getPersona (@PathVariable("id")Long id){
         Persona persona = iPersonaServ.findPersona(id);
         return new ResponseEntity<>(persona, HttpStatus.OK);
-    }
+    }   
     
     
-    /* @PreAuthorize("hasRole('ADMIN')")
-    @PostMapping("/persona/create")
-    public String createPersona(@RequestBody Persona persona){
-        iPersonaServ.savePersona(persona);
-        return "persona creada con exito";
-    }       */
-    
-    //@PreAuthorize("hasRole('ADMIN')")
       
-    @PutMapping("/persona/edit/{id}")    
+    @PutMapping("/persona/edit/")    
     //@CrossOrigin(origins = "https://portfoliolucasferreira.web.app/") 
     @CrossOrigin(origins = "http://localhost:4200") 
-    public ResponseEntity<Persona> editPersona(@PathVariable Long id,
-                                                @RequestParam("nombreApellido") String nuevoNomApe,
-                                                @RequestParam("cargo")String nuevoCargo,
-                                                @RequestParam("lugarNacimiento")String nuevoLugar,
-                                                @RequestParam("fechaNacimiento")String nuevoFechaNac,
-                                                @RequestParam("img")String nuevoImg,
-                                                @RequestParam("banner")String nuevoBanner,
-                                                @RequestParam("acerca")String nuevoAcerca){
-        Persona persona = iPersonaServ.findPersona(id);
+    public ResponseEntity<Persona> editPersona(@RequestBody Persona pers){
+        Persona persona = iPersonaServ.findPersona(pers.getId());
         
-        persona.setNombreApellido(nuevoNomApe);
-        persona.setCargo(nuevoCargo);
-        persona.setLugarNacimiento(nuevoBanner);
-        persona.setFechaNacimiento(nuevoFechaNac);
-        persona.setImg(nuevoImg);
-        persona.setBanner(nuevoBanner);
-        persona.setAcerca(nuevoAcerca);
+        persona.setNombreApellido(pers.getNombreApellido());
+        persona.setCargo(pers.getCargo());
+        persona.setLugarNacimiento(pers.getLugarNacimiento());
+        persona.setFechaNacimiento(pers.getFechaNacimiento());
+        persona.setImg(pers.getImg());
+        persona.setBanner(pers.getBanner());
+        persona.setAcerca(pers.getAcerca());
         
+        iPersonaServ.savePersona(persona);
+
         return new ResponseEntity<>(persona,HttpStatus.OK);
     }
 }
